75339137e41bdd6211f9c8a49116878335fcd406
danielmeier <danielme@student.ethz.ch>
2014-06-01 16:00:45 +0200

    reporting, changed to Hinf instead of H2

--- MATLAB/design_K_H2Syn.m
+++ MATLAB/design_K_H2Syn.m
@@ -11,5 +11,5 @@ Dyu = D_P(Iy,Iu);
 % H2_controller using h2syn()
 P_h2syn  = ss(A,[Bw,Bu], [Ce;Cy],[Dew, Deu;Dyw,Dyu]); 
 
-[K_h2syn, Gnom_2syn, gamma_2syn, info_2syn] = h2syn(P_h2syn, nmeas, nctrl);
+[K_h2syn, Gnom_2syn, gamma_2syn, info_2syn] = hinfsyn(P_h2syn, nmeas, nctrl);
 
